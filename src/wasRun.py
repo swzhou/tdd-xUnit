67eb44d881882464b33fcf01a60c41a09bbee475
Swing Zhou <swing.zhou@hotmail.com>
2012-05-27 09:58:33 +0800

    use log to track setUp and testMethod orders

--- src/wasRun.py
+++ src/wasRun.py
@@ -5,7 +5,8 @@ class WasRun(TestCase):
 		TestCase.__init__(self, name)
 	def setUp(self):
 		self.wasRun = None
-		self.wasSetUp = 1
+		self.log = "setUp "
 	def testMethod(self):
 		self.wasRun = 1
+		self.log = self.log + "testMethod "
 		
--- test/testCaseFacts.py
+++ test/testCaseFacts.py
@@ -4,13 +4,8 @@ from testCase import TestCase
 from wasRun import WasRun
 
 class TestCaseFacts(TestCase):
-	def setUp(self):
-		self.test = WasRun("testMethod")
-	def testRunning(self):
-		self.test.run()
-		assert(self.test.wasRun)
-	def testSetUp(self):
-		self.test.run()
-		assert(self.test.wasSetUp)
-TestCaseFacts("testRunning").run()
-TestCaseFacts("testSetUp").run()
+	def testTemplateMethod(self):
+		test = WasRun("testMethod")
+		test.run()
+		assert("setUp testMethod " == test.log)
+TestCaseFacts("testTemplateMethod").run()
